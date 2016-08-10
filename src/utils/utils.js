15c176e0054fc0ba1b48853e13169fc4afa51a70
Benjamin Nowack <mail@bnowack.de>
2016-08-10 12:02:02 +0200

    Extend utils with `throttle` and `animate` helpers (#utils)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -63,6 +63,54 @@ function($) {
 		},
         
         /**
+         * Throttles function execution
+         * 
+         * @param {number} delay - Release-delay in msec
+         * @param {function} callback - Callback
+         * @param {string|null} callbackId - Function identifier for throttling the callback
+         */
+		throttle: function(delay, callback, callbackId) {
+			var self = this;
+			if (callbackId) {
+				var eventId = 1 + Math.random();
+				if (this.timeouts[callbackId]) {// still blocked
+					this.timeouts[callbackId] = eventId;// set event id to indicate there was a blocked call
+				} else {
+					this.timeouts[callbackId] = eventId;
+					callback();
+					// release throttle
+					setTimeout(function() {
+						// call callback another time if there were blocked calls after this one, e.g. to make sure the last scroll position is applied, etc. 
+						if (self.timeouts[callbackId] !== eventId) {
+							self.debounce(delay, callback, callbackId);
+						}
+						self.timeouts[callbackId] = null;
+					}, delay);
+				}
+			} else {// throttle via animation frames
+				this.animate(callback);
+			}
+		},
+		
+		/**
+		 * Executes the callback when the browser's layout engine is ready for a redraw
+		 * 
+		 * @param {function} callback - Callback function
+		 * @param {string} callbackId - (optional) identifier for debouncing the callback
+		 */
+		animate: function(callback, callbackId) {
+			callbackId = callbackId || Math.random();
+			if (window.requestAnimationFrame) {
+				if (this.timeouts[callbackId]) {
+					cancelAnimationFrame(this.timeouts[callbackId]);
+				}
+				this.timeouts[callbackId] = requestAnimationFrame(callback);
+			} else {
+				this.debounce(1000 / 60, callback, callbackId);
+			}
+		},
+        
+        /**
          * Loads a file from the given url
          * 
          * @param {string} path - File URL
